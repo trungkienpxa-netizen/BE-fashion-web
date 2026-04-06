626c716d8ca6c72bbf2286bfc20f384bcee4c7b7
trungkienpxa-netizen <trungkienpxa@gmail.com>
2026-04-07 00:06:27 +0700

    update

--- src/main/java/edu/thanglong/FashionApplication.java
+++ src/main/java/edu/thanglong/FashionApplication.java
@@ -2,11 +2,9 @@ package edu.thanglong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableMongoAuditing
 @EnableScheduling
 public class FashionApplication {
     
